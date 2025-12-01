5167802c8e1d9fd7c62c157a974d232c05da9020
Vikash1440 <meenavikash34262@gmail.com>
2025-12-02 02:39:37 +0530

    login page

--- backend-DRF/api/urls.py
+++ backend-DRF/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path,include
 from accounts import views as UserViews
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 urlpatterns = [
 
     # Base API Endpoint
     path('register/',UserViews.RegisterView.as_view()),
+
+    # JWT API Endpoint
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
